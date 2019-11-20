86e3769fcc5744cd5b473d38c33aadd5ec19ca3f
wangfucheng <wangfucheng@machloop.com>
2019-11-20 11:08:49 +0800

    deploy

---
+++ precache-manifest.61e442ebceff503c0e5eafd91beddf56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7d929bc34f7d91143ad1068712600f",
+    "url": "/demo-hooks-movie-app/index.html"
+  },
+  {
+    "revision": "b51de4ec623e273b2cc3",
+    "url": "/demo-hooks-movie-app/static/css/main.85a47941.chunk.css"
+  },
+  {
+    "revision": "e8cac80ac97e19291639",
+    "url": "/demo-hooks-movie-app/static/js/2.2500c3d2.chunk.js"
+  },
+  {
+    "revision": "b51de4ec623e273b2cc3",
+    "url": "/demo-hooks-movie-app/static/js/main.be81ab0f.chunk.js"
+  },
+  {
+    "revision": "b6d549a490e743032964",
+    "url": "/demo-hooks-movie-app/static/js/runtime-main.ec8c009b.js"
+  }
+]);
